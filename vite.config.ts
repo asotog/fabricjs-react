79e31eff38adf2e81cf20491741df55d588beb02
Wenxin Mao <maowenxin72@yahoo.com>
2024-04-14 12:26:49 -0500

    build out app instead of lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,7 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      insertTypesEntry: true
-    })
-  ],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/lib/index.tsx'),
-      name: 'FabricJS React',
-      fileName: (format) => `fabricjs-react.${format}.js`
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom', 'fabric'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: 'React'
-        }
-      }
-    }
-  }
+  plugins: [react()],
 })
